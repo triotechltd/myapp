c54ddfc3b05d95fba1c5ff86b6bf286b27cf545f
Ahad Khalid <ahad.khalid@triotech.in>
2026-05-10 09:42:06 +0000

    fixed image pull policy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('GitOps Working by ahad khalid khan v1')
+  res.send('GitOps Working by ahad khalid khan v2')
 })
 
 app.listen(3000, () => {
