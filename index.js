68e121f945d35bb40bd56dfa924549e9ec9d2023
Ahad Khalid <ahad.khalid@triotech.in>
2026-05-10 09:35:52 +0000

    changes v2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('GitOps Working by ahad khalid khan')
+  res.send('GitOps Working by ahad khalid khan v1')
 })
 
 app.listen(3000, () => {
